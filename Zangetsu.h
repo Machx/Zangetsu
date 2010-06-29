9824f3a38138c07d095955610d7e5c712ad91b0e
Colin Wheeler <cocoasamurai@gmail.com>
2010-06-29 14:50:05 -0500

    Adding Framework header

---
+++ Zangetsu.h
@@ -0,0 +1,12 @@
+/*
+ *  Zangetsu.h
+ *  Zangetsu
+ *
+ *  Created by Colin Wheeler on 6/29/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "CWDebugLog.h"
+#import "CWMacros.h"
+#import "CWSHA1_Convenience.h"
