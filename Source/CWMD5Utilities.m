c737e2433016465d6e1b6bc3342e9bc4baf02146
Colin Wheeler <cocoasamurai@gmail.com>
2010-12-29 16:16:57 -0600

    md5 hash was cut short just a bit, this fixes that issue

--- Source/CWMD5Utilities.m
+++ Source/CWMD5Utilities.m
@@ -26,11 +26,11 @@
 	CC_MD5(cStringRep, (CC_LONG)strlen(cStringRep), md5Hash);
 	
 	NSString *shaHashString = [NSString  stringWithFormat:
-							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
 							   md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3], 
 							   md5Hash[4], md5Hash[5], md5Hash[6], md5Hash[7], 
 							   md5Hash[8], md5Hash[9], md5Hash[10], md5Hash[11], 
-							   md5Hash[12], md5Hash[13], md5Hash[14], md5Hash[15]];
+							   md5Hash[12], md5Hash[13], md5Hash[14], md5Hash[15],md5Hash[16]];
 	
 	return [shaHashString lowercaseString];
 }
@@ -51,11 +51,11 @@
 	CC_MD5(cStringRep, (CC_LONG)strlen(cStringRep), md5Hash);
 	
 	NSString *shaHashString = [NSString  stringWithFormat:
-							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
 							   md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3], 
 							   md5Hash[4], md5Hash[5], md5Hash[6], md5Hash[7], 
 							   md5Hash[8], md5Hash[9], md5Hash[10], md5Hash[11], 
-							   md5Hash[12], md5Hash[13], md5Hash[14], md5Hash[15]];
+							   md5Hash[12], md5Hash[13], md5Hash[14], md5Hash[15],md5Hash[16]];
 	
 	return [shaHashString lowercaseString];
 }
