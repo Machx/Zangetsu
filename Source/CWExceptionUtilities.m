5b2f2b67843a10455780a061a20d5575fcf5cef1
Colin Wheeler <cocoasamurai@gmail.com>
2011-02-03 20:47:30 -0600

    documenting methods in CWExceptionUtilities

--- Source/CWExceptionUtilities.m
+++ Source/CWExceptionUtilities.m
@@ -8,11 +8,19 @@
 
 #import "CWExceptionUtilities.h"
 
+/**
+ Quick method to make the NSApp report the exception
+ */
 void CWReportException(NSException *exception)
 {
 	[NSApp reportException:exception];
 }
 
+/**
+ Takes a NSException object and runs an Critical Alert Panel with the Exception 
+ information as well as the stack trace of where the stack trace occurred minus
+ the exception handling frames.
+ */
 void CWShowExceptionAsAlertPanel(NSException *exception)
 {
 	NSInteger result = NSRunCriticalAlertPanel(@"Application Error Occurred",
@@ -25,6 +33,10 @@ void CWShowExceptionAsAlertPanel(NSException *exception)
 
 @implementation NSException (CWNSExceptionAdditions)
 
+/**
+ Calls CWStackTrace() to return NSThreads callStackSymbols
+ and put them in string form.
+ */
 -(NSString *)cw_stackTrace
 {
 	return CWStackTrace();
