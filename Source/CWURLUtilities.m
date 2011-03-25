47757564f9a14eb91fb2d543fa4097fabfc0ad23
Colin Wheeler <cocoasamurai@gmail.com>
2011-03-24 21:33:59 -0500

    CWURLUtilties reformatting

--- Source/CWURLUtilities.m
+++ Source/CWURLUtilities.m
@@ -9,34 +9,32 @@
 #import "CWURLUtilities.h"
 
 /**
- Creates a NSURL with the string passed in
- 
- Creates a URL with the string passed in and escapes the necessary 
- url characters to create a NSURL object
- 
- @param url a NSString with the url you want to make a NSURL object from
- @return a NSURL object from the string passed in
+ * Creates a NSURL with the string passed in
+ *
+ * Creates a URL with the string passed in and escapes the necessary
+ * url characters to create a NSURL object
+ *
+ * @param url a NSString with the url you want to make a NSURL object from
+ * @return a NSURL object from the string passed in
  */
-NSURL* CWURL(NSString *url)
-{
-	NSURL *_urlValue = [NSURL URLWithString:[url cw_escapeEntitiesForURL]];
-	
-	return _urlValue;
+NSURL * CWURL(NSString * url){
+    NSURL * _urlValue = [NSURL URLWithString:[url cw_escapeEntitiesForURL]];
+
+    return _urlValue;
 }
 
 static NSString * kCWURLUtiltyErrorDomain = @"com.Zangetsu.CWURLUtilities";
 
 @implementation CWURLUtilities
 
-+(NSError *)errorWithLocalizedMessageForStatusCode:(NSInteger)code
-{
-	NSString *localizedMessage = [NSHTTPURLResponse localizedStringForStatusCode:code];
-	
-	if (localizedMessage) {
-		return CWCreateError(code,kCWURLUtiltyErrorDomain,localizedMessage);
-	}
-	
-	return nil;
++ (NSError *) errorWithLocalizedMessageForStatusCode:(NSInteger)code {
+    NSString * localizedMessage = [NSHTTPURLResponse localizedStringForStatusCode:code];
+
+    if (localizedMessage) {
+        return CWCreateError(code, kCWURLUtiltyErrorDomain, localizedMessage);
+    }
+
+    return nil;
 }
 
 @end
