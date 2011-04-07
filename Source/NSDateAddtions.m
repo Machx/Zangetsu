285da420860ab72bfe5750da5c3471374e5864bc
Colin Wheeler <cocoasamurai@gmail.com>
2011-04-07 15:29:24 -0500

    new api cw_dateByAddingMinutes:

--- Source/NSDateAddtions.m
+++ Source/NSDateAddtions.m
@@ -8,10 +8,15 @@
 
 #import "NSDateAddtions.h"
 
+static const NSTimeInterval kCWSecondsIn1Minute = 60;
 static const NSTimeInterval kCWSecondsIn1Hour = 3600;
 static const NSTimeInterval kCWSecondsIn1Day = 86400;
 
 @implementation NSDate (CWNSDateAddtions)
+
+-(NSDate *)cw_dateByAddingMinutes:(NSUInteger)minutes {
+	return [self dateByAddingTimeInterval:(kCWSecondsIn1Minute * minutes)];
+}
 
 -(NSDate *)cw_dateByAddingHours:(NSUInteger)hours {
 	return [self dateByAddingTimeInterval:(kCWSecondsIn1Hour * hours)];
