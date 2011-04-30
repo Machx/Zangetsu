18870ca0dc07e65a2ef692d97039a77cb946bd43
Colin Wheeler <cocoasamurai@gmail.com>
2011-04-29 21:50:03 -0500

    doing proper documentation for CWClassExists()

--- Source/CWFoundation.m
+++ Source/CWFoundation.m
@@ -9,8 +9,15 @@
 #import "CWFoundation.h"
 
 /**
+ Determines if an Objective-C Class exists in the Objective-C Runtime
+ 
  Until true weak linking classes comes to the Mac OS X & iOS SDKs
- this will be necessary to determine if a class exists or not
+ this will be necessary to determine if a class exists or not. Internally
+ this uses NSClassFromString() if a Class object is returned the class 
+ does exist in the runtime, otherwise it doesn't.
+ 
+ @param class a NSString with the name of an Objective-C Class
+ @return a BOOL with YES if the class exists or no if it doesn't
  */
 BOOL CWClassExists(NSString * class)
 {
