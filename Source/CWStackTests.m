7053247c6d6cf117dfa3d7d9ad6c98c49f094da9
Colin Wheeler <cocoasamurai@gmail.com>
2011-06-03 10:45:35 -0500

    adding a unit test for the CWStack block enumeration

--- Source/CWStackTests.m
+++ Source/CWStackTests.m
@@ -34,6 +34,30 @@
 	STAssertTrue([[stack1 description] isEqualToString:[stack2 description]], @"stacks should be equal");
 }
 
+-(void)testBasicEnumeration {
+	
+	NSArray *array = [NSArray arrayWithObjects:@"This",@"is",@"a",@"sentence", nil];
+	
+	CWStack *stack = [[CWStack alloc] initWithObjectsFromArray:array];
+	
+	__block NSInteger index = 0;
+	
+	[stack popToObject:@"This" withBlock:^(id obj) {
+		if (index == 0) {
+			STAssertTrue([(NSString *)obj isEqualToString:@"sentence"], @"strings should be equal");
+			index++;
+		} else if (index == 1) {
+			STAssertTrue([(NSString *)obj isEqualToString:@"a"], @"strings should be equal");
+			index++;
+		} else if (index == 2) {
+			STAssertTrue([(NSString *)obj isEqualToString:@"is"], @"strings should be equal");
+			index++;
+		} else {
+			STAssertTrue(FALSE, @"should not get here");
+		}
+	}];
+}
+
 - (void)tearDown
 {
     // Tear-down code here.
