671e57f1d63e823bf9f3c8ed2c61aac4c3e0c5cd
Colin Wheeler <cocoasamurai@gmail.com>
2011-06-28 14:57:17 -0500

    removing an unecessary log statement

--- Source/CWPathUtilitiesTests.m
+++ Source/CWPathUtilitiesTests.m
@@ -22,9 +22,7 @@
     
     NSString *homePath1 = [@"~/Documents/Test" stringByExpandingTildeInPath];
     NSString *homePath2 = [CWPathUtilities pathByAppendingHomeFolderPath:@"Documents/Test"];
-    
-    NSLog(@"\nhomepath1 %@\nhomepath2 %@",homePath1,homePath2);
-    
+     
     STAssertTrue([homePath1 isEqualToString:homePath2], @"paths should be equal");
 }
 
