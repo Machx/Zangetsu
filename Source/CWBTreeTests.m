8143dcbc31393faf59fa323703cfb8b0275c0fc6
Colin Wheeler <colindw@gmail.com>
2011-07-22 12:17:43 -0500

    adding Unit Test for CWBTreeNode equality
    [new] Unit Test for CWBTreeNode -isEqualTo method
    Unit test test for cor
    rect and incorrect equality among 2 nodes

--- Source/CWBTreeTests.m
+++ Source/CWBTreeTests.m
@@ -31,6 +31,21 @@
     STAssertTrue([[[tree1 rootNode] value] isEqual:[[tree2 rootNode] value]], @"Root nodes should be equal");
 }
 
+-(void)testNodeIsEqual {
+    
+    NSString *string1 = @"Hypnotoad";
+    
+    CWBTreeNode *node1 = [[CWBTreeNode alloc] initWithValue:string1];
+    
+    CWBTreeNode *node2 = node1;
+    
+    STAssertTrue([node1 isEqualTo:node2], @"Nodes should be equivalent");
+    
+    CWBTreeNode *node3 = [[CWBTreeNode alloc] initWithValue:@"Cheez it!"];
+    
+    STAssertFalse([node1 isEqualTo:node3], @"The 2 nodes have different values and should always be false");
+}
+
 - (void)tearDown
 {
     // Tear-down code here.
