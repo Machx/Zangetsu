f6620d78e2c861fdcf7ac28c239ca4e5f07b6d7e
Colin Wheeler <cocoasamurai@gmail.com>
2011-08-13 09:14:15 -0500

    adding CWMacroTests for testing the testable macros in CWMacros.h
    [new] CWMacroTests [new] unit test for NSDICT() [new] unit test for
    NSARRAY() [new] unit test for NSBOOL() [new] unit test for NSSET()

---
+++ Source/CWMacroTests.h
@@ -0,0 +1,13 @@
+//
+//  CWMacroTests.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/13/11.
+//  Copyright 2011. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWMacroTests : SenTestCase
+
+@end
---
+++ Source/CWMacroTests.m
@@ -0,0 +1,70 @@
+//
+//  CWMacroTests.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/13/11.
+//  Copyright 2011. All rights reserved.
+//
+
+#import "CWMacroTests.h"
+#import "CWMacros.h"
+
+@implementation CWMacroTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+-(void)testDictionaryMacro {
+    
+    const NSString *value1 = @"I'll be in the dome of understanding";
+    const NSString *key1 = @"aKey";
+    
+    NSDictionary *dict1 = NSDICT(value1,key1);
+    NSDictionary *dict2 = [NSDictionary dictionaryWithObject:value1 forKey:key1];
+    
+    STAssertTrue([dict1 isEqualToDictionary:dict2], @"Dictionaries should be the same");
+}
+
+-(void)testArrayMacro {
+    
+    const NSString *value1 = @"Fry";
+    const NSString *value2 = @"Bender";
+    const NSString *value3 = @"Leela";
+    
+    NSArray *array1 = NSARRAY(value1,value2,value3);
+    NSArray *array2 = [NSArray arrayWithObjects:value1,value2,value3, nil];
+    
+    STAssertTrue([array1 isEqualToArray:array2], @"Arrays should be equal");
+}
+
+-(void)testBoolMacro {
+    
+    NSNumber *bool1 = NSBOOL(YES);
+    BOOL bool2 = YES;
+    
+    STAssertTrue([bool1 boolValue] == bool2, @"BOOL values should be the same");
+}
+
+-(void)testSetMacro {
+    
+    const NSString *q1 = @"All Hail Hypnotoad!";
+    const NSString *q2 = @"We just need to hit the bullseye and all the dominos will fall like a house of cards... checkmate";
+    
+    NSSet *set1 = NSSET(q1,q2);
+    NSSet *set2 = [NSSet setWithObjects:q1,q2, nil];
+    
+    STAssertTrue([set1 isEqualToSet:set2], @"The 2 sets should be equal in content");
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
