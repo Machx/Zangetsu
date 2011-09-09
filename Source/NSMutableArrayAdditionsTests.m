55668d3180216a8f0408ae489fe9d0eb6cde5553
Colin Wheeler <cocoasamurai@gmail.com>
2011-09-09 16:13:53 -0500

    in the unit test, testing for making sure the copy items is actually copying items to a new address

--- Source/NSMutableArrayAdditionsTests.m
+++ Source/NSMutableArrayAdditionsTests.m
@@ -25,6 +25,13 @@
     [array2 cw_addObjectsFromArrayByCopying:array1];
     
     STAssertTrue([array1 isEqualTo:array2],@"Arrays should be equal");
+    
+    for (NSUInteger i = 0; i < [array1 count]; i++) {
+        id obj1 = [array1 objectAtIndex:i];
+        id obj2 = [array2 objectAtIndex:i];
+        
+        STAssertTrue(&obj1 != &obj2, @"shouldn't be at the same memory address if copied");
+    }
 }
 
 - (void)tearDown
