ced32f092637715ae53e1d4f4061b32d0a4d5b60
Colin Wheeler <cocoasamurai@gmail.com>
2011-11-27 11:01:01 -0600

    minor reformatting to make the code a bit more readable

--- Source/CWGraphicsFoundation.m
+++ Source/CWGraphicsFoundation.m
@@ -76,19 +76,19 @@ void CWAddRoundedRectToPath(CGContextRef context,
     }
 	
     CGContextSaveGState(context);
-	
-    CGContextTranslateCTM (context, CGRectGetMinX(rect),CGRectGetMinY(rect));
-    CGContextScaleCTM (context, ovalWidth, ovalHeight);
-	
-    fw = CGRectGetWidth (rect) / ovalWidth;
-    fh = CGRectGetHeight (rect) / ovalHeight;
-	
-    CGContextMoveToPoint(context, fw, fh/2);
-    CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
-    CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
-    CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
-    CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
-    CGContextClosePath(context);
-	
+	{
+		CGContextTranslateCTM (context, CGRectGetMinX(rect),CGRectGetMinY(rect));
+		CGContextScaleCTM (context, ovalWidth, ovalHeight);
+		
+		fw = CGRectGetWidth (rect) / ovalWidth;
+		fh = CGRectGetHeight (rect) / ovalHeight;
+		
+		CGContextMoveToPoint(context, fw, fh/2);
+		CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
+		CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
+		CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
+		CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
+		CGContextClosePath(context);
+	}
     CGContextRestoreGState(context);
 }
