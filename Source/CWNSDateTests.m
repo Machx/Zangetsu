42440856d865104c023554942aa8e6d2627645c4
Colin Wheeler <cocoasamurai@gmail.com>
2012-03-10 12:22:58 -0600

    updating the date unit tests to deal with DST correctly

--- Source/CWNSDateTests.m
+++ Source/CWNSDateTests.m
@@ -37,26 +37,35 @@
 	NSDate *now = [NSDate date];
 	
 	NSDate *minutesFromNow1 = [now cw_dateByAddingMinutes:3 usingCalendar:nil];
-	NSDate *minutesFromNow2 = [now dateByAddingTimeInterval:180];
+	
+	NSDateComponents *components = [[NSDateComponents alloc] init];
+	[components setMinute:3];
+	NSDate *minutesFromNow2 = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:now options:0];
 	
 	STAssertTrue([minutesFromNow1 compare:minutesFromNow2] == NSOrderedSame, @"Dates should be the same");
 }
 
--(void)testDateByAddingHours {
-	
+-(void)testDateByAddingHours 
+{	
 	NSDate *now = [NSDate date];
 	
 	NSDate *hourFromNow1 = [now cw_dateByAddingHours:2 usingCalendar:nil];
-	NSDate *hourFromNow2 = [now dateByAddingTimeInterval:7200];
+	
+	NSDateComponents *components = [[NSDateComponents alloc] init];
+	[components setHour:2];
+	NSDate *hourFromNow2 = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:now options:0];
 	
 	STAssertTrue([hourFromNow1 compare:hourFromNow2] == NSOrderedSame, @"Dates should be the same");
 }
 
--(void)testDateByAddingDays {
-	
+-(void)testDateByAddingDays 
+{
 	NSDate *today = [NSDate date];
 	
-	NSDate *tomorrow = [today dateByAddingTimeInterval:86400];
+	NSDateComponents *components = [[NSDateComponents alloc] init];
+	[components setDay:1];
+	NSDate *tomorrow = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:today options:0];
+	
 	NSDate *tomorrow2 = [today cw_dateByAddingDays:1 usingCalendar:nil];
 	
 	STAssertTrue([tomorrow compare:tomorrow2] == NSOrderedSame, @"Dates should be the same");
