e52d3f33bf30cfae7833ceb4bebc665a524276f4
Colin Wheeler <cocoasamurai@gmail.com>
2012-04-16 09:10:19 -0500

    basic unit test for CWTrie

--- Source/CWTrieTests.m
+++ Source/CWTrieTests.m
@@ -3,11 +3,27 @@
 //  Zangetsu
 //
 //  Created by Colin Wheeler on 4/15/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "CWTrieTests.h"
+#import "CWTrie.h"
+#import "CWAssertionMacros.h"
 
 @implementation CWTrieTests
+
+-(void)testBasicSetAndFind
+{
+	CWTrie *aTrie = [[CWTrie alloc] init];
+	
+	NSString *aKey = @"Hello";
+	NSString *aValue = @"World";
+	
+	[aTrie setObject:aValue withkey:aKey];
+	
+	NSString *foundValue = [aTrie findObjectWithKey:aKey];
+	
+	CWAssertEqualsStrings(aValue, foundValue);
+}
 
 @end
