713ee5ad8f5ba71729d30d0f4b8643062e7e6f4a
Colin Wheeler <cocoasamurai@gmail.com>
2012-04-18 19:05:19 -0500

    gcov code coverage added for unit tests

--- Source/CWTrie.h
+++ Source/CWTrie.h
@@ -30,7 +30,9 @@
 #import <Foundation/Foundation.h>
 
 @interface CWTrie : NSObject
+
 @property(nonatomic,assign) BOOL caseSensitive;
+
 -(id)objectValueForKey:(NSString *)aKey;
 -(void)setObjectValue:(id)aObject 
 			   forKey:(NSString *)aKey;
