427a41d12c78ec1197b5db2eb6c9ef81f0abbd2e
Colin Wheeler <cocoasamurai@gmail.com>
2012-04-22 12:11:00 -0500

    documenting CWTrie

--- Source/CWTrie.h
+++ Source/CWTrie.h
@@ -31,10 +31,45 @@
 
 @interface CWTrie : NSObject
 
+/**
+ Sets the Trie to be case sensitive or not for looking up/setting keys
+ 
+ By default Tries are case sensitive.
+ */
 @property(nonatomic,assign) BOOL caseSensitive;
 
+/**
+ Returns the object value for a given key
+ 
+ If a given key exists in a trie instance then this method will return
+ the corresponding value for that key. Otherwise this method will
+ return nil if there is no value for the corresponding key or if the
+ key doesn't exist in the trie instance.
+ 
+ @param aKey a NSString that corresponds to a key in the trie
+ @return the corresponding value to a given key or nil
+ */
 -(id)objectValueForKey:(NSString *)aKey;
+
+/**
+ Sets a object value corresponding to the given key
+ 
+ This stores the value in a Trie format for a given key. For example
+ If we were to store the value 1 for the key "Tent" and 2 for "Tennis"
+ the node layout would look like
+ 
+ [Root] -> [T] -> [e] -> [n] -> [t(1)]
+                           \ -> [n] -> [i] -> [s(2)]
+ */
 -(void)setObjectValue:(id)aObject 
 			   forKey:(NSString *)aKey;
+
+/**
+ Removes a object value for a given key
+ 
+ This method essentially calls [self setObjectValue:nil forKey:aKey]
+ setting nil for a given key
+ */
 -(void)removeObjectValueForKey:(NSString *)aKey;
+
 @end
