334bf2c3907ce2e6541b1022a1351713a61abe93
Colin Wheeler <cocoasamurai@gmail.com>
2012-04-27 10:31:19 -0500

    unit tests to test CWLinkedList

--- Source/CWLinkedListTests.h
+++ Source/CWLinkedListTests.h
@@ -3,7 +3,7 @@
 //  Zangetsu
 //
 //  Created by Colin Wheeler on 4/26/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- Source/CWLinkedListTests.m
+++ Source/CWLinkedListTests.m
@@ -3,11 +3,37 @@
 //  Zangetsu
 //
 //  Created by Colin Wheeler on 4/26/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "CWLinkedListTests.h"
+#import "CWLinkedList.h"
+#import "CWAssertionMacros.h"
 
 @implementation CWLinkedListTests
+
+-(void)testBasicLinkList
+{
+	CWLinkedList *list = [[CWLinkedList alloc] init];
+	
+	[list addObject:@"Hello"];
+	
+	STAssertNotNil([list objectAtIndex:0],@"Should contain an object");
+	CWAssertEqualsStrings([list objectAtIndex:0], @"Hello");
+	STAssertTrue(list.count == 1,@"count is incorrect");
+	
+	[list addObject:@"World"];
+	
+	STAssertNotNil([list objectAtIndex:1],@"Should contain an object");
+	CWAssertEqualsStrings([list objectAtIndex:1], @"World");
+	STAssertTrue(list.count == 2,@"count is incorrect");
+	
+	[list removeObjectAtIndex:0];
+	STAssertTrue(list.count == 1,@"count is incorrect");
+	CWAssertEqualsStrings([list objectAtIndex:0], @"World");
+	
+	[list removeObjectAtIndex:0];
+	STAssertTrue(list.count == 0,@"count is incorrect");
+}
 
 @end
