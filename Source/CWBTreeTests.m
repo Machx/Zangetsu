b10fc4e4f2d25ff388985076bb9a19393f9f0a0e
Colin Wheeler <cocoasamurai@gmail.com>
2012-05-03 17:30:00 -0500

    enumerate a btree with semi random values between 1 & 100

--- Source/CWBTreeTests.m
+++ Source/CWBTreeTests.m
@@ -52,6 +52,34 @@
 	STAssertTrue(total == 120,@"total should be correct if enumeratedcorrectly");
 }
 
+-(void)testRandomBTreeEnumeration
+{
+	CWBTree *btree = [[CWBTree alloc] init];
+	
+	[btree setObjectValue:CWNSINT(50) forKey:@"50"];
+	[btree setObjectValue:CWNSINT(10) forKey:@"10"];
+	[btree setObjectValue:CWNSINT(3) forKey:@"3"];
+	[btree setObjectValue:CWNSINT(90) forKey:@"90"];
+	[btree setObjectValue:CWNSINT(55) forKey:@"55"];
+	[btree setObjectValue:CWNSINT(2) forKey:@"2"];
+	[btree setObjectValue:CWNSINT(9) forKey:@"9"];
+	[btree setObjectValue:CWNSINT(77) forKey:@"77"];
+	[btree setObjectValue:CWNSINT(99) forKey:@"99"];
+	[btree setObjectValue:CWNSINT(19) forKey:@"19"];
+	[btree setObjectValue:CWNSINT(1) forKey:@"1"];
+	[btree setObjectValue:CWNSINT(12) forKey:@"12"];
+	[btree setObjectValue:CWNSINT(85) forKey:@"85"];
+	[btree setObjectValue:CWNSINT(17) forKey:@"17"];
+	
+	__block NSUInteger total = 0;
+	
+	[btree enumerateOverObjectsWithBlock:^(id value, NSString *aKey, BOOL *stop) {
+		total += [(NSNumber *)value intValue];
+	}];
+	
+	STAssertTrue(total == 529,@"count is not correct");
+}
+
 -(void)testNilValueForNonExistantkey
 {
 	CWBTree *btree = [[CWBTree alloc] init];
