bc522c802c00d5ae2790c5a7d812062c4d0d3e84
Colin Wheeler <cocoasamurai@gmail.com>
2012-05-15 09:03:12 -0500

    adding a unit test to test -removeObject and -removeObjectAtIndex:

--- Source/CWDoublyLinkedListTests.m
+++ Source/CWDoublyLinkedListTests.m
@@ -43,4 +43,33 @@
 	CWAssertEqualsStrings([list objectAtIndex:3], @"Super");
 }
 
+-(void)testRemoveObject
+{
+	CWDoublyLinkedList *list = [[CWDoublyLinkedList alloc] init];
+	
+	[list addObject:@"Fry"];
+	[list addObject:@"Leela"];
+	[list addObject:@"Bender"];
+	[list addObject:@"Hypnotoad"];
+	
+	STAssertTrue(list.count == 4,@"Incorrect count");
+	CWAssertEqualsStrings([list objectAtIndex:0], @"Fry");
+	CWAssertEqualsStrings([list objectAtIndex:1], @"Leela");
+	CWAssertEqualsStrings([list objectAtIndex:2], @"Bender");
+	CWAssertEqualsStrings([list objectAtIndex:3], @"Hypnotoad");
+	
+	[list removeObject:@"Bender"];
+	
+	STAssertTrue(list.count == 3,@"Incorrect count");
+	CWAssertEqualsStrings([list objectAtIndex:0], @"Fry");
+	CWAssertEqualsStrings([list objectAtIndex:1], @"Leela");
+	CWAssertEqualsStrings([list objectAtIndex:2], @"Hypnotoad");
+	
+	[list removeObjectAtIndex:1];
+	
+	STAssertTrue(list.count == 2,@"Incorrect count");
+	CWAssertEqualsStrings([list objectAtIndex:0], @"Fry");
+	CWAssertEqualsStrings([list objectAtIndex:1	], @"Hypnotoad");
+}
+
 @end
