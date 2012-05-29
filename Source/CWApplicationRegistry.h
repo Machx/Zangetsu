7cbb383e173df7d0071fdc7fdcdc11a30475b8e2
Colin Wheeler <cocoasamurai@gmail.com>
2012-05-29 11:27:11 -0500

    moving CWApplicationRegistry documentation to the header

--- Source/CWApplicationRegistry.h
+++ Source/CWApplicationRegistry.h
@@ -33,11 +33,60 @@
 static const NSInteger kPidNotFound = -1;
 
 @interface CWApplicationRegistry : NSObject
-
+/**
+ Returns a BOOL indicating if a given application is running
+ 
+ Searches all the applications listed as running and if the application is 
+ found then returns YES, otherwise returns NO.
+ 
+ @param appName a NSString with the app name you wish to check
+ @return a BOOL with YES if the app is running, otherwise NO
+ */
 +(BOOL)applicationIsRunning:(NSString *)appName;
+
+/**
+ Returns the pid for a running application
+ 
+ Searches all the applications listed as running and if the application is 
+ found then returns its pid, otherwise returns kPidNotFound (-1).
+ 
+ @param appName a NSString with the name of the app whose pid you want
+ @return a NSInteger with the pid or kPidNotFound (-1) if not found
+ */
 +(NSInteger)pidForApplication:(NSString *)appName;
+
+/**
+ Returns the bundle identifier for a running application
+ 
+ Searches for all the applications listed as running and if the application is
+ running then it returns the apps bundle identifier, otherwise returns nil.
+ 
+ @param appName a NSString with the name of the application whose bundle identifier you want
+ @return a NSString with the bundle identifier of the app name passed in or nil if the app isn't running
+ */
 +(NSString *)bundleIdentifierForApplication:(NSString *)appName;
+
+/**
+ Returns the NSRunningApplication instance for an App
+ 
+ Search all the applications running and if the application is found then 
+ this returns the NSRunningApplication intance corresponding to that particular
+ application.
+ 
+ @param appName a NSString with the name of the application whose corresponding NSRunningApplication you want
+ @return the NSRunningApplication instance corresponding to appName, otherwise nil
+ */
 +(NSRunningApplication *)runningAppInstanceForApp:(NSString *)appName;
+
+/**
+ Returns the NSImage Icon for the Application corresponding to appname if its running
+ 
+ Searches all the running applications and if it finds an application with the same name
+ returns a reference to the particular applications icon in NSImage form.
+ 
+ @param appName a NSString representing the application whose instance you want its icon data
+ @return a NSImage corresponding to appName, otherwise nil
+ */
 +(NSImage *)iconForApplication:(NSString *)appName;
 @end
 #endif
--- Source/CWApplicationRegistry.m
+++ Source/CWApplicationRegistry.m
@@ -32,15 +32,7 @@
 
 @implementation CWApplicationRegistry
 
-/**
- Returns a BOOL indicating if a given application is running
- 
- Searches all the applications listed as running and if the application is 
- found then returns YES, otherwise returns NO.
- 
- @param appName a NSString with the app name you wish to check
- @return a BOOL with YES if the app is running, otherwise NO
- */
+
 +(BOOL)applicationIsRunning:(NSString *)appName
 {
 	__block BOOL isRunning = NO;
@@ -55,16 +47,6 @@
 	return isRunning;
 }
 
-
-/**
- Returns the pid for a running application
- 
- Searches all the applications listed as running and if the application is 
- found then returns its pid, otherwise returns kPidNotFound (-1).
- 
- @param appName a NSString with the name of the app whose pid you want
- @return a NSInteger with the pid or kPidNotFound (-1) if not found
- */
 +(NSInteger)pidForApplication:(NSString *)appName
 {
 	__block NSInteger pid = kPidNotFound;
@@ -80,15 +62,6 @@
 	return pid;
 }
 
-/**
- Returns the bundle identifier for a running application
- 
- Searches for all the applications listed as running and if the application is
- running then it returns the apps bundle identifier, otherwise returns nil.
- 
- @param appName a NSString with the name of the application whose bundle identifier you want
- @return a NSString with the bundle identifier of the app name passed in or nil if the app isn't running
- */
 +(NSString *)bundleIdentifierForApplication:(NSString *)appName
 {
 	__block NSString *bundleIdentifier = nil;
@@ -103,16 +76,6 @@
 	return bundleIdentifier;
 }
 
-/**
- Returns the NSRunningApplication instance for an App
- 
- Search all the applications running and if the application is found then 
- this returns the NSRunningApplication intance corresponding to that particular
- application.
- 
- @param appName a NSString with the name of the application whose corresponding NSRunningApplication you want
- @return the NSRunningApplication instance corresponding to appName, otherwise nil
- */
 +(NSRunningApplication *)runningAppInstanceForApp:(NSString *)appName
 {
     __block NSRunningApplication *appInstance = nil;
@@ -127,15 +90,6 @@
     return appInstance;
 }
 
-/**
- Returns the NSImage Icon for the Application corresponding to appname if its running
- 
- Searches all the running applications and if it finds an application with the same name
- returns a reference to the particular applications icon in NSImage form.
- 
- @param appName a NSString representing the application whose instance you want its icon data
- @return a NSImage corresponding to appName, otherwise nil
- */
 +(NSImage *)iconForApplication:(NSString *)appName 
 {    
     __block NSImage *appIcon = nil;
