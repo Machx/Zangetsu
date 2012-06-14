684c4d664d78c6df25c61363e6678c81b391bd9a
Colin Wheeler <cocoasamurai@gmail.com>
2012-06-14 09:27:35 -0500

    starting to document CWBTree

--- Source/CWBTree.h
+++ Source/CWBTree.h
@@ -42,17 +42,63 @@ typedef NSUInteger (^BTreeNodeValue)(id obj);
 
 @interface CWBTree : NSObject
 
+/**
+ The Node Evaluator is a Block you can set so it can return a NSUInteger from a node value
+ 
+ @param (block) id obj the object in the BTree whose NSUInteger value is needed
+ */
 @property(copy) BTreeNodeValue nodeValueEvaluator;
 
+/**
+ Inserts the value into the BTree 
+ 
+ The BTree uses the node evaluator to get the objects NSUInteger value and then inserts
+ the object into the appropriate point in the BTree. IF this value is nil then this 
+ method just immmediately returns and does nothing.
+ 
+ @param value the object you want inserted into the BTree
+ */
 -(void)insertValue:(id)value;
 
+/**
+ Enumerates the BTree in a given mode, passing a block over each node/value it passes
+ 
+ @param mode this must be either CWBTreeNodeBreadthFirstSearch or CWBTreeNodeDepthFirstSearch
+ otherwise this method just exits. If one of the modes is matched then it enumerates 
+ over the btree according to the algorithm pattern. 
+ 
+ @param mode the node to enumerate over all the nodes in 
+ @param (block) id value the value of the node being enumerated over
+ @param (block) node the CWBTreeNode being enumerated over
+ @param (block) stop a BOOL pointer you can set to YES to stop the enumeration
+ */
 -(void)enumerateBTreeInMode:(CWBTreeEnumerationMode)mode 
 				  withBlock:(void (^)(id value, CWBTreeNode *node, BOOL *stop))block;
 
+/**
+ This method enumerates over the BTree in Breadth First Search
+ 
+ @param mode the node to enumerate over all the nodes in 
+ @param (block) id value the value of the node being enumerated over
+ @param (block) node the CWBTreeNode being enumerated over
+ @param (block) stop a BOOL pointer you can set to YES to stop the enumeration
+ */
 -(void)enumerateBTreeWithBlock:(void (^)(id value, CWBTreeNode *node, BOOL *stop))block;
 
+/**
+ This method gets the NSUInteger value for the object and returns YES if its in the Tree, otherwise returns NO
+ 
+ @param object the object you wish to see if it is in the tree
+ @return a BOOL with YES if the obejct is in the tree, otherwise returns NO
+ */
 -(BOOL)isObjectInTree:(id)object;
 
+/**
+ This method is the same to -isObjectInTree but uses a NSUInteger node value
+ 
+ @param nodevalue the nodevalue of an object you wish to see if it is in the tree
+ @return a BOOL with YES if the obejct is in the tree, otherwise returns NO
+ */
 -(BOOL)isNodeValueInTree:(NSUInteger)nodeValue;
 
 @end
