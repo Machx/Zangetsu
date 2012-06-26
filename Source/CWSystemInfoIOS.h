1f5a83b8a8a10504395183e750b88df3d7b74684
Colin Wheeler <cocoasamurai@gmail.com>
2012-06-26 10:26:44 -0500

    documenting the new api's in CWSystemInfoIOS

--- Source/CWSystemInfoIOS.h
+++ Source/CWSystemInfoIOS.h
@@ -57,10 +57,25 @@ static NSString * const kCWSystemBugFixVersion = @"bugfixVersion";
  */
 +(NSInteger)cpuCoreCount;
 
+/**
+ Returns a hardware model string such as 'iPad2,1' that uniquely idenifies the host machine
+ 
+ @return a NSString that identifies the hardware that the code is being executed on
+ */
 +(NSString *)hardwareModelString;
 
+/**
+ Performs the same function as +hardwareModelString except this returns strings like 'iPhone 4S' instead of iPhone4,1
+ 
+ @return a human readable string such 'iPhone 4S' that identifies the hardware this code is being run on
+ */
 +(NSString *)englishHardwareString;
 
+/**
+ Returns YES if Retina Resolution is supported on the host hardware, otherwise returns NO
+ 
+ @return a BOOL indicating YES is retina resolution is supported on the current hardware, otherwise NO
+ */
 +(BOOL)retinaSupported;
 
 @end
