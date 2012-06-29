208c71065cda4fc5f0724240d0938926734d14e2
Colin Wheeler <cocoasamurai@gmail.com>
2012-06-29 10:55:02 -0500

    documenting -cw_imageResizedToSize:withInterpolationQuality

--- Source/UIImageAdditions.h
+++ Source/UIImageAdditions.h
@@ -34,11 +34,24 @@
 /**
  Resizes a UIImage to the new passed in size
  
+ This method is equivalent to calling 
+ -cw_imageResizedToSize:size withInterpolationQuality:kCGInterpolationHigh.
+ 
  @param size The size you wish the UIImage receiver to be resized to.
  @return a new UIImage resized to the desired size
  */
 -(UIImage *)cw_imageResizedToSize:(CGSize)size;
 
+/**
+ Resizes a UIImage to the new passed in size
+ 
+ This method allows you to resize an image to a specified size and also
+ to control the interpolation quality on the new image
+ 
+ @param size The size you wish the UIImage receiver to be resized to.
+ @param quality the quality of the interpolation to be done in the resize
+ @return a new UIImage resized to the desired size
+ */
 -(UIImage *)cw_imageResizedToSize:(CGSize)size 
 		 withInterpolationQuality:(CGInterpolationQuality)quality;
 
