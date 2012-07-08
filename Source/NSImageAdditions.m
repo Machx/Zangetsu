38df852b5fb92afd9bc4be312d0de7aa90df2136
Colin Wheeler <cocoasamurai@gmail.com>
2012-07-08 10:14:06 -0500

    NSImage addition -cw_resizeToSize:

---
+++ Source/NSImageAdditions.m
@@ -0,0 +1,65 @@
+/*
+//  NSImage+CW.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 7/7/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+Copyright (c) 2012 Colin Wheeler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#import "NSImageAdditions.h"
+
+@implementation NSImage (CWNSImageAdditions)
+
+-(NSImage *)cw_imageResizedToSize:(CGSize)size
+{
+	assert(self != nil);
+	
+	NSRect currentRect = NSMakeRect(0.0f, 0.0f, self.size.height, self.size.height);
+	CGImageRef image = [self CGImageForProposedRect:&currentRect
+											context:[NSGraphicsContext currentContext]
+											  hints:nil];
+	CGRect rect = { CGPointZero, size };
+	
+	CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 
+												 CGImageGetBitsPerComponent(image), 0,
+												 CGImageGetColorSpace(image), 
+												 kCGImageAlphaPremultipliedLast);
+	if (context == NULL) {
+		CWDebugLog(@"ERROR: Received NULL CGContextRef");
+		return nil;
+	}
+	
+	CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+	CGContextDrawImage(context, rect, image);
+	
+	CGImageRef cgImage = CGBitmapContextCreateImage(context);
+	NSImage *nsimage = [[NSImage alloc] initWithCGImage:cgImage size:size];
+	
+	CGImageRelease(cgImage);
+	CGContextRelease(context);
+	
+	return nsimage;
+}
+
+@end
