0271bcdfa7ec5e65bfa998ef0beb9140ae894d75
Colin Wheeler <cocoasamurai@gmail.com>
2012-07-10 08:11:57 -0500

    documenting cw_imageResizedToSize: and cw_imageResizedToSize:withInterpolationQuality:

--- Source/NSImageAdditions.h
+++ Source/NSImageAdditions.h
@@ -31,8 +31,24 @@
 
 @interface NSImage (CWNSImageAdditions)
 
+/**
+ Returns a new image of the receiver resized to the specified size
+ 
+ This method is equivalent to calling 
+ cw_imageResizedToSize:size withInterPolationQuality:kCGInterpolationHigh
+ 
+ @param size a CGSize spec that the object should be resized to
+ @return a new NSImage object that has been scaled to the new size or nil if something went wrong
+ */
 -(NSImage *)cw_imageResizedToSize:(CGSize)size;
 
+/**
+ Returns a new image of the receiver resized to the specified size
+ 
+ @param size a CGSize spec that the object should be resized to
+ @param quality the amount of interpolation quality that should be applied to the resize
+ @return a new NSImage object that has been scaled to the new size or nil if something went wrong
+ */
 -(NSImage *)cw_imageResizedToSize:(CGSize)size 
 		 withInterpolationQuality:(CGInterpolationQuality)quality;
 
