3e89245a84f8f275ea5b24e3ebaeeb45a45197a8
Colin Wheeler <cocoasamurai@gmail.com>
2012-07-15 12:05:03 -0500

    moving some macros into CWAssertionMacros.h to make things clean

--- Source/CWAssertionMacros.h
+++ Source/CWAssertionMacros.h
@@ -29,6 +29,64 @@ THE SOFTWARE.
 
 #import <SenTestingKit/SenTestingKit.h>
 
+#ifdef DEBUG
+#	define CWAssert(cond,desc) NSAssert(cond,desc)
+#	define CWAssert1(cond,desc,a1) NSAssert1(cond,desc,a1)
+#	define CWAssert2(cond,desc,a1,a2) NSAssert2(cond,desc,a1,a2)
+#	define CWAssert3(cond,desc,a1,a2,a3) NSAssert3(cond,desc,a1,a2,a3)
+#	define CWAssert4(cond,desc,a1,a2,a3,a4) NSAssert4(cond,desc,a1,a2,a3,a4)
+#	define CWAssert5(cond,desc,a1,a2,a3,a4,a5) NSAssert5(cond,desc,a1,a2,a3,a4,a5)
+#else
+#	define CWAssert(...) /**/
+#	define CWAssert1(...) /**/
+#	define CWAssert2(...) /**/
+#	define CWAssert3(...) /**/
+#	define CWAssert4(...) /**/
+#	define CWAssert5(...) /**/
+#endif
+
+#define CWAssertV(cond, ...) \
+do { \
+	if(!cond) { \
+		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
+																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
+														  lineNumber:__LINE__ \
+														 description:__VA_ARGS__]; \
+	} \
+} while(0);
+
+/* Basis of this is from Stack Overflow
+ http://stackoverflow.com/questions/2283987/xcode-call-stack-trace-on-assert
+ Basically a simple assertion that spits the exact expression that failed out to log and
+ also spits out the stack trace without the assertion handling frames
+ */
+#define CWAssertST(x,desc) \
+do { \
+	if (!(x)) { \
+		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
+																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
+														  lineNumber:__LINE__ \
+														 description:[NSString stringWithFormat:@"%s:%s failed assertion\nMessage:%@\n%@",__PRETTY_FUNCTION__, #x, desc, [NSThread callStackSymbols]]]; \
+	} \
+} while(0);
+
+#define CWIBOutletAssert(_x_) \
+do { \
+	if(_x_ == nil) { \
+		NSLog(@"IBOutlet Assertion: %s is nil and appears to not be hooked up!",#_x_); \
+	} \
+} while(0);
+
+#define CWZeroLengthAssert(_x_) \
+do { \
+	if([_x_ length] == 0) { \
+		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
+																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
+														  lineNumber:__LINE__ \
+														 description:[NSString stringWithFormat:@"%s length is 0 (needs to be > 0)",#_x_]]; \
+	} \
+} while(0);
+
 /**
  Throws an assertion when the objects are equal
  */
--- Source/CWMacros.h
+++ Source/CWMacros.h
@@ -53,67 +53,6 @@
 #define CWDebugLocationString() [NSString stringWithFormat:@"%s[%i]",__PRETTY_FUNCTION__,__LINE__]
 
 //MARK: -
-//MARK: NSAssert Functions
-
-#ifdef DEBUG
-#	define CWAssert(cond,desc) NSAssert(cond,desc)
-#	define CWAssert1(cond,desc,a1) NSAssert1(cond,desc,a1)
-#	define CWAssert2(cond,desc,a1,a2) NSAssert2(cond,desc,a1,a2)
-#	define CWAssert3(cond,desc,a1,a2,a3) NSAssert3(cond,desc,a1,a2,a3)
-#	define CWAssert4(cond,desc,a1,a2,a3,a4) NSAssert4(cond,desc,a1,a2,a3,a4)
-#	define CWAssert5(cond,desc,a1,a2,a3,a4,a5) NSAssert5(cond,desc,a1,a2,a3,a4,a5)
-#else
-#	define CWAssert(...) /**/
-#	define CWAssert1(...) /**/
-#	define CWAssert2(...) /**/
-#	define CWAssert3(...) /**/
-#	define CWAssert4(...) /**/
-#	define CWAssert5(...) /**/
-#endif
-
-#define CWAssertV(cond, ...) \
-do { \
-	if(!cond) { \
-		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
-																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
-														  lineNumber:__LINE__ \
-														 description:__VA_ARGS__]; \
-	} \
-} while(0);
-
-/* Basis of this is from Stack Overflow
- http://stackoverflow.com/questions/2283987/xcode-call-stack-trace-on-assert
- Basically a simple assertion that spits the exact expression that failed out to log and
- also spits out the stack trace without the assertion handling frames
- */
-#define CWAssertST(x,desc) \
-do { \
-	if (!(x)) { \
-		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
-																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
-														  lineNumber:__LINE__ \
-														 description:[NSString stringWithFormat:@"%s:%s failed assertion\nMessage:%@\n%@",__PRETTY_FUNCTION__, #x, desc, [NSThread callStackSymbols]]]; \
-	} \
-} while(0);
-
-#define CWIBOutletAssert(_x_) \
-do { \
-	if(_x_ == nil) { \
-		NSLog(@"IBOutlet Assertion: %s is nil and appears to not be hooked up!",#_x_); \
-	} \
-} while(0);
-
-#define CWZeroLengthAssert(_x_) \
-do { \
-	if([_x_ length] == 0) { \
-		[[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] \
-																file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] \
-														  lineNumber:__LINE__ \
-														 description:[NSString stringWithFormat:@"%s length is 0 (needs to be > 0)",#_x_]]; \
-	} \
-} while(0);
-
-//MARK: -
 //MARK: GCD Macros
 
 #define CWGCDQueueLow() dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW,0)
