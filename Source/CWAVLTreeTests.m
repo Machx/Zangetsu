3bc511ac7358ff04efd163dc6a1fdc9fa22db27b
Colin Wheeler <cocoasamurai@gmail.com>
2012-08-01 09:33:01 -0500

    adding unit tests for CWAVLTree

--- Source/CWAVLTreeTests.m
+++ Source/CWAVLTreeTests.m
@@ -7,7 +7,27 @@
 //
 
 #import "CWAVLTreeTests.h"
+#import "CWAVLTree.h"
 
 @implementation CWAVLTreeTests
+
+-(void)testBasicInsertion
+{
+	/**
+	 Tree should look like
+		 42
+	   /----\
+	   8    50
+	 /---\
+	 2   9              */
+	
+	CWAVLTree *tree = [[CWAVLTree alloc] init];
+	
+	[tree addObject:@42];
+	[tree addObject:@50];
+	[tree addObject:@8];
+	[tree addObject:@2];
+	[tree addObject:@9];
+}
 
 @end
