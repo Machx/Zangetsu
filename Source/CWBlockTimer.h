a806328e6011d3907aa2081c0e470b022b9a05b2
Colin Wheeler <cocoasamurai@gmail.com>
2012-08-14 09:27:54 -0500

    documenting CWBlockTimer

--- Source/CWBlockTimer.h
+++ Source/CWBlockTimer.h
@@ -31,14 +31,38 @@
 
 @interface CWBlockTimer : NSObject
 
+/**
+ Returns the Internal NSTimer Instance that CWBlockTimer instances manage
+ 
+ @return the Internal NSTimer instance
+ */
 @property(readonly,retain) NSTimer *internalTimer;
 
+/**
+ Invokes the block that the timer is set to fire
+ */
 -(void)fire;
 
+/**
+ Invalidates the NSTimer instance
+ */
 -(void)invalidate;
 
+/**
+ Returns if the internal NSTimer instance is valid
+ 
+ @return a BOOL indicating if the timer instance is valid
+ */
 -(BOOL)isValid;
 
+/**
+ Creates a new CWBlockTimer instance intiailized with the passed in values
+ 
+ @param interval a NSTimeInterval for how long the timer action is supposed to wait till firing
+ @param block the block to be executed when the timer fires
+ @param repeats a BOOL indicating if the timer should repeat
+ @return a new CWBlockTimer instance configured with the parameters passed in
+ */
 +(CWBlockTimer *)timerWithTimeInterval:(NSTimeInterval)interval
 								 block:(dispatch_block_t)block
 							   repeats:(BOOL)repeats;
