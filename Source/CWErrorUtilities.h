045631ce440a2edd07037a4a5046b5e6e26f3b9e
Colin Wheeler <cocoasamurai@gmail.com>
2012-09-09 10:44:07 -0500

    new documentation for CWCreateErrorWithUserInfo() and updated for CWCreateError()

--- Source/CWErrorUtilities.h
+++ Source/CWErrorUtilities.h
@@ -34,18 +34,33 @@
 static NSString * const kCWErrorDomain = @"CWErrorDomain";
 
 /**
- * Convenience method for creating a NSError Object
- *
- * Easy convenience method to create a NSError Object. It checks for the error message
- * and throws an assertion if it's missing, allows for a string with formatting and 
- * passing arguments for the formatting of a string. If no domain is passed in it defaults
- * to kCWErrorDomain.
- * 
- * @param domain a NSString specifying the domain for a NSError object
- * @param errorCode a NSInteger for the error code in a NSError object
- * @param errorMessageFormat a NSString with optional formatting which specifies the NSError NSLocalizedDescriptionKey
- * @return a NSError object with the values passed in
+ Convenience method for creating a NSError Object
+ 
+ Easy convenience method to create a NSError Object. It checks for the error message
+ and throws an assertion if it's missing, allows for a string with formatting and
+ passing arguments for the formatting of a string. If no domain is passed in it defaults
+ to kCWErrorDomain.
+ 
+ @param domain a NSString specifying the domain for a NSError object
+ @param errorCode a NSInteger for the error code in a NSError object
+ @param errorMessageFormat a NSString with optional formatting which specifies the NSError NSLocalizedDescriptionKey
+ @return a NSError object with the values passed in
  */
 NSError * CWCreateError(NSString * domain, NSInteger errorCode, NSString * errorMessageFormat, ...);
 
+/**
+ Convenience method for creating a NSError Object
+ 
+ Easy convenience method to create a NSError Object. This method is similar to CWCreateError(),
+ but it also allows setting userInfo dictionary entries. It checks for the error message
+ and throws an assertion if it's missing, allows for a string with formatting and
+ passing arguments for the formatting of a string. If no domain is passed in it defaults
+ to kCWErrorDomain.
+ 
+ @param domain a NSString specifying the domain for a NSError object
+ @param errorCode a NSInteger for the error code in a NSError object
+ @param info a NSDictionary with any other key/value pairs to be added to the NSError object
+ @param errorMessageFormat a NSString with optional formatting which specifies the NSError NSLocalizedDescriptionKey
+ @return a NSError object with the values passed in
+ */
 NSError * CWCreateErrorWithUserInfo(NSString * domain, NSInteger errorCode, NSDictionary *info, NSString * errorMessageFormat, ...);
