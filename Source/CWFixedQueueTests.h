3085d67c48a543b7027dba66e0f96351d973a03f
Colin Wheeler <cocoasamurai@gmail.com>
2012-09-12 08:56:44 -0500

    unit tests for CWFixedQueue

---
+++ Source/CWFixedQueueTests.h
@@ -0,0 +1,13 @@
+//
+//  CWFixedQueueTests.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 9/11/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWFixedQueueTests : SenTestCase
+
+@end
---
+++ Source/CWFixedQueueTests.m
@@ -0,0 +1,58 @@
+//
+//  CWFixedQueueTests.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 9/11/12.
+//
+//
+
+#import "CWFixedQueueTests.h"
+#import "CWFixedQueue.h"
+#import "CWAssertionMacros.h"
+
+@implementation CWFixedQueueTests
+
+-(void)testBasicQueueProperties
+{
+	CWFixedQueue *queue = [CWFixedQueue new];
+	queue.capacity = 2;
+	
+	STAssertTrue([queue count] == 0, nil);
+	[queue enqueue:@"Good"];
+	STAssertTrue([queue count] == 1, nil);
+	[queue enqueue:@"News"];
+	STAssertTrue([queue count] == 2, nil);
+	[queue enqueue:@"Everybody!"];
+	STAssertTrue([queue count] == 2, nil);
+	
+	[queue enumerateContents:^(id object, NSUInteger index, BOOL *stop) {
+		switch (index) {
+			case 0:
+				CWAssertEqualsStrings(object,@"News");
+				break;
+			case 1:
+				CWAssertEqualsStrings(object, @"Everybody!");
+				break;
+			default:
+				STFail(@"Enumerated past collection objects bounds");
+				break;
+		}
+	}];
+	
+	NSString *result = [queue dequeue];
+	CWAssertEqualsStrings(result,@"News");
+	
+	result = [queue dequeue];
+	CWAssertEqualsStrings(result, @"Everybody!");
+}
+
+-(void)testEnqueueObjectsFromArray
+{
+	CWFixedQueue *queue = [CWFixedQueue new];
+	queue.capacity = 2;
+	
+	[queue enqueueObjectsInArray:@[ @"Nope",@"Everybody Watch",@"Hypnotoad" ]];
+	STAssertTrue([queue count] == 2, nil);
+}
+
+@end
