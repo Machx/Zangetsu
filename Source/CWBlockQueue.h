063605156d9485ff9591111b6fc16811dc9ebfff
Colin Wheeler <cocoasamurai@gmail.com>
2012-10-21 12:02:07 -0500

    tidying up and adding to the lacking parts of the documentation for CWBlockQueue

--- Source/CWBlockQueue.h
+++ Source/CWBlockQueue.h
@@ -45,7 +45,7 @@
 /**
  Returns a new CWBlockOperation object
  
- @param block the block to be used with the operation object. This must not be nil.
+ @param block the block to be used with the operation object. Must not be nil.
  @return a CWBlocKOperation instance
  */
 +(CWBlockOperation *)operationWithBlock:(dispatch_block_t)block;
@@ -58,40 +58,54 @@
 
 typedef enum : NSUInteger {
 	kCWBlockQueueTargetPrivateQueue = 0,      //private GCD Queue
-	kCWBlockQueueTargetMainQueue = 1,         // dispatch blocks onto the main queue
-	kCWBlockQueueTargetGCDLowPriority = 2,    // DISPATCH_QUEUE_PRIORITY_LOW queue
-	kCWBlockQueueTargetGCDNormalPriority = 3, // DISPATCH_QUEUE_PRIORITY_NORMAL queue
-	kCWBlockQueueTargetGCDHighPriority = 4    // DISPATCH_QUEUE_PRIORITY_HIGH queue
+	kCWBlockQueueTargetMainQueue = 1,         //GCD Main Queue
+	kCWBlockQueueTargetGCDLowPriority = 2,    //GCD Low Global Priority Queue
+	kCWBlockQueueTargetGCDNormalPriority = 3, //GCD Normal Global Priority queue
+	kCWBlockQueueTargetGCDHighPriority = 4    //GCD High Global Priority queue
 } CWBlockQueueTargetType;
 
 @interface CWBlockQueue : NSObject
 /**
  Returns a global CWBlockQueue object initialized to point at the GCD Main Queue
+ 
+ @return mainQueue a CWBlockQueue object using the GCD Main Queue
  */
 +(CWBlockQueue *)mainQueue;
 
 /**
- Returns a global CWBlockQueue object initialized to point at the GCD Default Priority Queue
+ Returns a global CWBlockQueue object initialized to point at the GCD Default
+ Priority Queue
+ 
+ @return globalDefaultQueue a CWBlock queue object initialized and using the GCD
+ Default Priority Queue
  */
 +(CWBlockQueue *)globalDefaultQueue;
 
 /**
- Creates a Temporary Queue that you can use to throw a block on for execution in the background
+ Creates a Temporary Queue that you can use to throw a block on for execution in
+ the background
  
- This creates a temporary dispatch_queue_t and then asynchronously executes the block and when 
- the block has completed executing the queue is immediately released. The temporary queue has a 
- unique label like 'com.Zangetsu.CWBlockQueue_TemporaryQueue_1A5F...' etc. 
+ This creates a temporary dispatch_queue_t and then asynchronously executes the
+ block and when the block has completed executing the queue is immediately 
+ released. The temporary queue has a unique label like 
+ 'com.Zangetsu.CWBlockQueue_TemporaryQueue_1A5F...' etc.
+ 
+ @param block the block to be executed on a temporary queue
  */
 +(void)executeBlockOnTemporaryQueue:(dispatch_block_t)block;
 
 /**
  Initializes a CWBlockQueue object with a queue type and label as applicable
  
- @param type a NSInteger with the type of the queue to be used as defined in the header
- @param concurrent a BOOL thats used if the private queue type is specified to mark the private queue as serial or concurrent
- @param label a NSString thats used for the queue label if a private queue type is specified
+ @param type the Queue type to use for dispatching blocks on
+ @param concurrent a BOOL set to YES for concurrent queue or NO for serial
+ @param label if a private queue is used this labels the queue with your label
+ 
+ @return an initialed CWBlockQueue
  */
--(id)initWithQueueType:(CWBlockQueueTargetType)type concurrent:(BOOL)concurrent label:(NSString *)qLabel;
+-(id)initWithQueueType:(CWBlockQueueTargetType)type
+			concurrent:(BOOL)concurrent
+				 label:(NSString *)qLabel;
 
 /**
  Initialzes a CWBlockQueue object with a specified gcd queue
@@ -115,48 +129,58 @@ typedef enum : NSUInteger {
 /**
  Sets the CWBlockQueues gcd queue to target another gcd queue.
  
- If nil is passed in this method does nothing.
+ @param blockQueue the queue to target, if nil this does nothing
  */
 -(void)setTargetCWBlockQueue:(CWBlockQueue *)blockQueue;
 
 /**
  Sets the CWBlockQueues gcd queue to target another gcd queue.
  
- If NULL is passed in this method does nothing.
+ @param GCDQueue to be targeted by this queue, if NULL this does nothing
  */
 -(void)setTargetGCDQueue:(dispatch_queue_t)GCDQueue;
 
 /**
  Adds an operation object & asynchronously executes it
  
- The operations operation block is executed first then the completion block (if its set)
+ The operations operation block is executed first then the completion block
+ 
+ @param operation a CWBlockOperation object
  */
 -(void)addOperation:(CWBlockOperation *)operation;
 
 /**
  Asnchronously executes a block on the gcd queue
+ 
+ @param block a dispatch_block_t type to be executed on the queue
  */
 -(void)addoperationWithBlock:(dispatch_block_t)block;
 
 /**
  Synchronously executes a CWBlockOperation object
  
- The operations operation block is executed first then the completion block (if its set)
+ The operations operation block is executed first then the completion block
+ 
+ @param operation a CWBlockOperation object to be executed Synchronously
  */
 -(void)addSynchronousOperation:(CWBlockOperation *)operation;
 
 /**
  Synchronously executes a block on the gcd queue
+ 
+ @param block a dispatch_block_t to be executed Synchronously
  */
 -(void)addSynchronousOperationWithBlock:(dispatch_block_t)block;
 
 /**
- Asynchonously waits for all already queued blocks to finish executing and then executes the passed in block
+ Asynchronous operation that waits for the queue to finish & then executes block
+ 
+ @param block the dispatch_block_t to be executed when all others have finished
  */
 -(void)executeWhenQueueIsFinished:(dispatch_block_t)block;
 
 /**
- Synchronously waits for the queue to finish executing before continuing execution
+ Waits for the queue to finish executing all previously queued blocks
  */
 -(void)waitForQueueToFinish;
 
