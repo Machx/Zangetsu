f979967bfcab7671b432e0326699271368ff0ac6
Alex Gray <alex@mrgray.com>
2012-10-24 16:13:37 -0400

    changes?

--- Source/CWDateUtilitiesTests.m
+++ Source/CWDateUtilitiesTests.m
@@ -6,25 +6,6 @@
 //  Copyright 2012. All rights reserved.
 //
  
- Copyright (c) 2012 Colin Wheeler
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
  */
 
 #import "CWDateUtilitiesTests.h"
@@ -47,9 +28,9 @@
 	 appropriate description back
 	 */
 	NSString *dateString = CWDateStringFromComponents(2012, 6, 9, 14, 0, 0, nil, [NSCalendar currentCalendar]);
-	
-	NSString *dateString2 = @"2012-06-09 14:00:00 -0500";
-	
+	NSLog(@"Date 1: %@", dateString);
+	NSString *dateString2 = @"2012-06-09 14:00:00 -0400";
+	NSLog(@"Date 2: %@", dateString2);
 	CWAssertEqualsStrings(dateString, dateString2);
 }
 
