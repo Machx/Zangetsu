b0ddd3a221ac42bd7b7e0e2ff7a321ebf30c36a9
Colin Wheeler <cocoasamurai@gmail.com>
2013-01-04 09:50:29 -0600

    giving the NSDictionaryAdditions Documentation some much needed love

--- Source/NSDictionaryAdditions.h
+++ Source/NSDictionaryAdditions.h
@@ -33,24 +33,44 @@
 @interface NSDictionary (CWNSDictionaryAdditions)
 
 /**
- Ruby Inspired Iterator for NSDictionary in Objective-C
+ Ruby Inspired Enumerator for NSDictionary Key/Value Pairs in Objective-C
+ 
+ @param key the current key being enumerated over
+ @param value the value corresponding to the current key being enumerated over
+ @param stop BOOL pointer which you can set to YES to stop enumeration
  */
 -(void)cw_each:(void (^)(id key, id value, BOOL *stop))block;
 
 /**
- Same as cw_each but operates concurrently and passes in a bool
- pointer allowing you to stop the enumeration
+ Concurrent Enumeration of NSDictionary Key/Value pairs
+ 
+ This method is equivalent to enumerating using the Foundation API 
+ -enumerateKeysAndObjectsWithOptions passing in NSEnumerationConcurrent
+ 
+ @param key the current key being enumerated over
+ @param value the value corresponding to the current key being enumerated over
+ @param stop BOOL pointer which you can set to YES to stop enumeration
  */
 -(void)cw_eachConcurrentlyWithBlock:(void (^)(id key, id value, BOOL *stop))block;
 
 /**
- Simple Convenience method to tell if the dictionary
- contains a particular key
+ Returns a BOOL inicating if the key passed in is contained in the receiver
+ 
+ @return a BOOL with YES if the key was found in the receiver, otherwise NO
  */
 -(BOOL)cw_containsKey:(NSString *)key;
 
 /**
- An dictionary mapping method using only 1 block
+ Map one dictionary to another
+ 
+ This works by enumerating over the contents of a NSDictionary and allows you to
+ return a new NSDictionary object from the block whose key/value pairs should be
+ added to the dictionary returned from this method. If you do not want to map 
+ any key/value pair to the dictionary, then simply return nil in the block.
+ 
+ @param key the key currently being enumerated over
+ @param value the value corresponding to the key currently being enumerated over
+ @param a NSDictionary with the keys/values returned from the block calls
  */
 -(NSDictionary *)cw_mapDictionary:(NSDictionary* (^)(id key, id value))block;
 
