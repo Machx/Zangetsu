bf49de3c7c648c8ebb84e46dc4ff8cd1123e1af7
Colin Wheeler <cocoasamurai@gmail.com>
2013-01-05 10:00:44 -0600

    cleaning up lots of documentation to clip to 80 characters wide to conform to the conventions

--- Source/NSDateAddtions.h
+++ Source/NSDateAddtions.h
@@ -35,8 +35,9 @@
 /**
  Returns a new date object with the same date as the original but n minutes ahead
  
- @param minutes a NSInteger with the number of minutes you want the new date object to advance by
- @param dateCal a NSCalendar object you want to be used for the date calculation or nil if you want [NSCalendar currentCalendar]
+ @param minutes the number of minutes you want the new date object to advance by
+ @param dateCal a NSCalendar object you want to be used for the date calculation
+		or nil if you want [NSCalendar currentCalendar]
  @return a new NSDate object advanced forward by n minutes
  */
 -(NSDate *)cw_dateByAddingMinutes:(NSInteger)minutes usingCalendar:(NSCalendar *)dateCal;
@@ -44,8 +45,9 @@
 /**
  Returns a new date object with the same date as the original but n hours ahead
  
- @param hours a NSInteger with the number of hours you want the new date object to advance by
- @param dateCal a NSCalendar object you want to be used for the date calculation or nil if you want [NSCalendar currentCalendar]
+ @param hours the number of hours you want the new date object to advance by
+ @param dateCal a NSCalendar object you want to be used for the date calculation
+		or nil if you want [NSCalendar currentCalendar]
  @return a new NSDate object advanced forward by n hours
  */
 -(NSDate *)cw_dateByAddingHours:(NSInteger)hours usingCalendar:(NSCalendar *)dateCal;
@@ -53,8 +55,9 @@
 /**
  Returns a new date object with the same date as the original but n days ahead
  
- @param days a NSInteger with the number of days you want the new date object to advance by
- @param dateCal a NSCalendar object you want to be used for the date calculation or nil if you want [NSCalendar currentCalendar]
+ @param days number of days you want the new date object to advance by
+ @param dateCal a NSCalendar object you want to be used for the date calculation
+		or nil if you want [NSCalendar currentCalendar]
  @return a new NSDate object advanced forward by n days
  */
 -(NSDate *)cw_dateByAddingDays:(NSInteger)days usingCalendar:(NSCalendar *)dateCal;
