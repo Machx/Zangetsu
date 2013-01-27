4186094a5f330d5f262f1eb5994316be7e35d0e3
Colin Wheeler <cocoasamurai@gmail.com>
2013-01-27 13:08:00 -0600

    cleanup CWExceptionUtilities

--- Source/CWExceptionUtilities.m
+++ Source/CWExceptionUtilities.m
@@ -30,21 +30,18 @@
 #import "CWExceptionUtilities.h"
 
 #if Z_HOST_OS_IS_MAC_OS_X
-void CWShowExceptionAsAlertPanel(NSException *exception)
-{
+void CWShowExceptionAsAlertPanel(NSException *exception) {
 	NSInteger result = NSRunCriticalAlertPanel(@"Application Error Occurred",
-											   [NSString stringWithFormat:@"Uncaught Exception: %@\n%@\n%@",[exception name], [exception reason],[exception cw_stackTrace]],
+											   [NSString stringWithFormat:@"Uncaught Exception: %@\n%@\n%@",
+												[exception name], [exception reason],[exception cw_stackTrace]],
 											   @"Continue", @"Quit", nil);
-	if (result == NSAlertAlternateReturn) {
-		[[NSApplication sharedApplication] terminate:nil];
-	}
+	if (result == NSAlertAlternateReturn) [[NSApplication sharedApplication] terminate:nil];
 }
 #endif
 
 @implementation NSException (CWNSExceptionAdditions)
 
--(NSString *)cw_stackTrace
-{
+-(NSString *)cw_stackTrace {
 	return CWStackTrace();
 }
 
