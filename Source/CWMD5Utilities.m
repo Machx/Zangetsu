63dbf2eb042b9b3fcb14f2727a1ff6ccf20d1a1f
Colin Wheeler <cocoasamurai@gmail.com>
2013-01-28 09:27:31 -0600

    cleanup CWMD5Utilities

--- Source/CWMD5Utilities.m
+++ Source/CWMD5Utilities.m
@@ -32,12 +32,12 @@
 
 @implementation NSString (CWMD5Utilities)
 
-+(NSString *)cw_md5HashFromString:(NSString *)str
-{
-	const char *cStringRep = [str UTF8String];
++(NSString *)cw_md5HashFromString:(NSString *)string {
+	NSParameterAssert(string);
+	const char *cStringRep = [string UTF8String];
 	unsigned char md5Hash[CC_MD5_DIGEST_LENGTH];
-	
 	CC_MD5(cStringRep, (CC_LONG)strlen(cStringRep), md5Hash);
+	
 	NSString *md5HashString = [NSString  stringWithFormat:
 							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
 							   md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3], md5Hash[4],
@@ -52,13 +52,13 @@
 
 @implementation NSData (CWMD5Utilities)
 
--(NSString *)cw_md5StringFromData
-{
-	NSString *str = [[NSString alloc] initWithData:self encoding:NSUTF8StringEncoding];
+-(NSString *)cw_md5StringFromData {
+	NSString *str = [[NSString alloc] initWithData:self
+										  encoding:NSUTF8StringEncoding];
 	const char *cStringRep = [str UTF8String];
 	unsigned char md5Hash[CC_MD5_DIGEST_LENGTH];
-	
 	CC_MD5(cStringRep, (CC_LONG)strlen(cStringRep), md5Hash);
+	
 	NSString *md5HashString = [NSString  stringWithFormat:
 							   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
 							   md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3], md5Hash[4],
