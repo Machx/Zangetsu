281a26a88b93ac40d52d1820f0aebe434ac4a011
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-01 09:15:18 -0600

    cleanup NSURLConnectionAdditions

--- Source/NSURLConnectionAdditions.m
+++ Source/NSURLConnectionAdditions.m
@@ -34,19 +34,15 @@
 
 +(void)cw_performAsynchronousRequest:(NSURLRequest *)request 
 						  onGCDQueue:(dispatch_queue_t)queue 
-					 completionBlock:(void (^)(NSData *data, NSURLResponse *response, NSError *error))block
-{
+					 completionBlock:(void (^)(NSData *data, NSURLResponse *response, NSError *error))block {
 	NSParameterAssert(request);
 	NSParameterAssert(queue);
-	
 	dispatch_async(queue, ^{
-		
 		NSURLResponse *_response = nil;
 		NSError *_urlError;
 		NSData *_data = [NSURLConnection sendSynchronousRequest:request
 											  returningResponse:&_response 
 														  error:&_urlError];
-		
 		dispatch_async(dispatch_get_main_queue(), ^{
 			block(_data,_response,_urlError);
 		});
@@ -55,24 +51,18 @@
 
 +(NSData *)cw_performGCDSynchronousRequest:(NSURLRequest *)request 
 								  response:(NSURLResponse **)response 
-								  andError:(NSError **)error
-{
+								  andError:(NSError **)error {
 	NSParameterAssert(request);
-	
 	__block NSData *data = nil;
 	__block NSURLResponse *resp = nil;
 	__block NSError *err;
-	
 	dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 		data = [NSURLConnection sendSynchronousRequest:request 
 									 returningResponse:&resp 
 												 error:&err];
 	});
-	
-	*response = resp;
 	if(response) *response = resp;
 	if(error) *error = err;
-	
 	return data;
 }
 
