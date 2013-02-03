5eb3c9f8b1b98eb685cf3ca9ecf2ef8a08748144
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-03 10:15:47 -0600

    cleanup NSColorAdditions

--- Source/NSColorAdditions.m
+++ Source/NSColorAdditions.m
@@ -31,11 +31,13 @@
 
 @implementation NSColor (CWNSColorAdditions)
 
--(CGColorRef)cw_CGColor
-{
+-(CGColorRef)cw_CGColor {
 	NSColor *nscolor = [self colorUsingColorSpaceName:NSCalibratedRGBColorSpace];
 	CGFloat components[4];
-	[nscolor getRed:&components[0] green:&components[1] blue:&components[2] alpha:&components[3]];
+	[nscolor getRed:&components[0]
+			  green:&components[1]
+			   blue:&components[2]
+			  alpha:&components[3]];
 	CGColorSpaceRef space = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
 	CGColorRef cgColor = CGColorCreate(space, components);
 	CGColorSpaceRelease(space);
