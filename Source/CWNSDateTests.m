d4e3f170976f19bd00c345b42474f5b2fea4e76f
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-04 09:23:07 -0600

    refactoring CWNSDateTests

--- Source/CWNSDateTests.m
+++ Source/CWNSDateTests.m
@@ -32,24 +32,24 @@
 
 @implementation CWNSDateTests
 
--(void)testDateByAddingMinutes
-{	
+-(void)testDateByAddingMinutes {
 	NSDate *now = [NSDate date];
-	
 	NSDate *minutesFromNow1 = [now cw_dateByAddingMinutes:3 usingCalendar:nil];
 	
 	NSDateComponents *components = [[NSDateComponents alloc] init];
 	components.minute = 3;
-	NSDate *minutesFromNow2 = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:now options:0];
+	NSDate *minutesFromNow2 = [[NSCalendar currentCalendar] dateByAddingComponents:components
+																			toDate:now
+																		   options:0];
 	
 	STAssertTrue([minutesFromNow1 compare:minutesFromNow2] == NSOrderedSame, @"Dates should be the same");
 }
 
--(void)testDateByAddingHours 
-{	
+-(void)testDateByAddingHours {
 	NSDate *now = [NSDate date];
 	
-	NSDate *hourFromNow1 = [now cw_dateByAddingHours:2 usingCalendar:nil];
+	NSDate *hourFromNow1 = [now cw_dateByAddingHours:2
+									   usingCalendar:nil];
 	
 	NSDateComponents *components = [[NSDateComponents alloc] init];
 	components.hour = 2;
@@ -58,15 +58,17 @@
 	STAssertTrue([hourFromNow1 compare:hourFromNow2] == NSOrderedSame, @"Dates should be the same");
 }
 
--(void)testDateByAddingDays 
-{
+-(void)testDateByAddingDays {
 	NSDate *today = [NSDate date];
 	
 	NSDateComponents *components = [[NSDateComponents alloc] init];
 	components.day = 1;
-	NSDate *tomorrow = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:today options:0];
+	NSDate *tomorrow = [[NSCalendar currentCalendar] dateByAddingComponents:components
+																	 toDate:today
+																	options:0];
 	
-	NSDate *tomorrow2 = [today cw_dateByAddingDays:1 usingCalendar:nil];
+	NSDate *tomorrow2 = [today cw_dateByAddingDays:1
+									 usingCalendar:nil];
 	
 	STAssertTrue([tomorrow compare:tomorrow2] == NSOrderedSame, @"Dates should be the same");
 }
