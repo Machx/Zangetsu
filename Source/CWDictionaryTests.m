00ecae7bf20d0a3aaf11356890f4220f17653564
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-04 09:32:08 -0600

    introduce new objc goodness to the unit tests

--- Source/CWDictionaryTests.m
+++ Source/CWDictionaryTests.m
@@ -32,13 +32,12 @@
 
 @implementation CWDictionaryTests
 
--(void)testContainsKey
-{
+-(void)testContainsKey {
 	/**
 	 Test for cw_dictionaryContainsKey to make sure it works properly. In this
 	 case it should return true for finding the object in the dictionary.
 	 */
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"foo",@"bar",nil];
+	NSDictionary *dictionary = @{ @"bar" : @"foo" };
 	
 	STAssertTrue([dictionary cw_containsKey:@"bar"],@"Dictionary should contain key bar");
 	
@@ -48,13 +47,12 @@
 	STAssertFalse([dictionary cw_containsKey:@"Zapp Brannigan"],@"Dictionary should not contain the key Zapp Brannigan");
 }
 
--(void)testDictionaryMapping
-{
+-(void)testDictionaryMapping {
 	/**
 	 make sure that mapping goes correctly. Again this test does 1-to-1 mapping of
 	 1 dictionary from another so if it goes correctly the dictionaries should be identical.
 	 */
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"foo",@"bar",nil];
+	NSDictionary *dictionary = @{ @"bar" : @"foo" };
 	
 	NSDictionary *results = [dictionary cw_mapDictionary:^NSDictionary *(id key, id value) {
 		return [NSDictionary dictionaryWithObjectsAndKeys:value,key, nil];
@@ -67,27 +65,26 @@
 	 new dictionary so we are going to create 3 key pairs and test to make sure 1 of them
 	 isn't being mapped so we know that the mapping method handles nil returns.
 	 */
-	NSDictionary *dictionary2 = [NSDictionary dictionaryWithObjectsAndKeys:@"Fry",@"Leela",
-								 @"Kif",@"Amy",
-								 @"Hermes",@"LaBarbara", nil];
+	NSDictionary *dictionary2 = @{ @"Leela" : @"Fry",
+								@"Amy" : @"Kif",
+								@"LaBarbara" : @"Hermes" };
 	
 	NSDictionary *results2 = [dictionary2 cw_mapDictionary:^NSDictionary *(id key, id value) {
 		if ([key isEqualToString:@"LaBarbara"]) { return nil; }
-		return [NSDictionary dictionaryWithObjectsAndKeys:value,key, nil];
+		return @{ key : value };
 	}];
 	
 	STAssertFalse([results2 cw_containsKey:@"LaBarbara"],@"Shouldn't contain LaBarbara if mapped correctly");
 }
 
--(void)testEach
-{
+-(void)testEach {
 	/**
 	 test cw_each for NSDictionary by enumerating all values in 1 dictioanry &
 	 settings those values in another dictionary. If enumeration was done correctly
 	 those values should be exactly the same.
 	 */
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Fry",@"Futurama",
-								@"McCloud",@"Highlander", nil];
+	NSDictionary *dictionary = @{ @"Futurama" : @"Fry",
+							   @"Highlander" : @"McCloud" };
 	
 	__block NSMutableDictionary *results = [[NSMutableDictionary alloc] init];
 	
@@ -98,15 +95,14 @@
 	STAssertEqualObjects(dictionary, results, @"Dictionaries should have the same contents if enumerated correctly");
 }
 
--(void)testEachStopPointer
-{
+-(void)testEachStopPointer {
 	/**
 	 make sure the BOOL *stop pointer is respected in cw_each and so we start enumeration
 	 & quit immediately after incrementing a counter. If done correclty the counter should
 	 only increment once.
 	 */
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Fry",@"Futurama",
-								@"McCloud",@"Highlander", nil];
+	NSDictionary *dictionary = @{ @"Futurama" : @"Fry",
+							   @"Highlander" : @"McCloud" };
 	__block NSUInteger count = 0;
 	
 	[dictionary cw_each:^(id key, id value, BOOL *stop) {
@@ -117,14 +113,13 @@
 	STAssertTrue(count == 1, @"Count should only be 1 if the stop pointer was respected");
 }
 
--(void)testEachConcurrent
-{
+-(void)testEachConcurrent {
 	/**
 	 make sure that concurrent each is working correctly by mapping a dictionary 
 	 and comparing them to make sure that the contents are identical.
 	 */
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Fry",@"Futurama",
-								@"McCloud",@"Highlander", nil];
+	NSDictionary *dictionary = @{ @"Futurama" : @"Fry",
+							   @"Highlander" : @"McCloud" };
 	
 	__block NSMutableDictionary *results = [[NSMutableDictionary alloc] init];
 	
