7c43e1b914822edfadb4f3eb352ac7fa0b57f391
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-08 08:53:47 -0600

    NSObject Tests converted to Specta/Expecta

--- Source/CWNSObjectTests.h
+++ Source/CWNSObjectTests.h
@@ -28,8 +28,3 @@
  */
 
 #import <SenTestingKit/SenTestingKit.h>
-
-
-@interface CWNSObjectTests : SenTestCase {}
-
-@end
--- Source/CWNSObjectTests.m
+++ Source/CWNSObjectTests.m
@@ -31,43 +31,38 @@
 #import "NSObjectAdditions.h"
 #import "CWAssertionMacros.h"
 
+SpecBegin(CWNSObject)
 
-@implementation CWNSObjectTests
+describe(@"-cw_associateValue:withKey:", ^{
+	it(@"should be able to store & retrieve strong references", ^{
+		char *key1 = "key1";
+		NSObject *object = [[NSObject alloc] init];
+		[object cw_associateValue:@"All Hail the Hypnotoad"
+						  withKey:key1];
+		
+		expect([object cw_valueAssociatedWithKey:key1]).to.equal(@"All Hail the Hypnotoad");
+	});
+});
 
-/**
- Testing the strong associated reference to make sure it works
- */
--(void)testStrongReferenceObjcAssociation
-{
-	char *key1 = "key1";
-	
-	NSObject *object = [[NSObject alloc] init];
-	
-	[object cw_associateValue:@"All Hail the Hypnotoad" 
-					  withKey:key1];
-	
-	CWAssertEqualsStrings([object cw_valueAssociatedWithKey:key1], @"All Hail the Hypnotoad");
-}
+describe(@"cw_associateWeakValue:withKey:", ^{
+	it(@"should be able to store & retrieve weak references", ^{
+		char *key3 = "key3";
+		NSObject *object = [[NSObject alloc] init];
+		[object cw_associateWeakValue:@"Hypnotoad Season 3"
+							  withKey:key3];
+		
+		expect([object cw_valueAssociatedWithKey:key3]).to.equal(@"Hypnotoad Season 3");
+	});
+});
 
--(void)testWeakObjcAssociation
-{
-	char *key3 = "key3";
-	
-	NSObject *object = [[NSObject alloc] init];
-	
-	[object cw_associateWeakValue:@"Hypnotoad Season 3"
-						  withKey:key3];
-	
-	CWAssertEqualsStrings([object cw_valueAssociatedWithKey:key3], @"Hypnotoad Season 3");
-}
+describe(@"-cw_isNotNil", ^{
+	it(@"should correctly identify nil and non nil references", ^{
+		id object1 = nil;
+		NSString *string = @"今日の天気がいいですね";
+		
+		expect([object1 cw_isNotNil]).to.beFalsy();
+		expect([string cw_isNotNil]).to.beTruthy();
+	});
+});
 
--(void)testNotNil
-{
-	id object1 = nil;
-	NSString *string = @"今日の天気がいいですね";
-	
-	STAssertFalse([object1 cw_isNotNil], nil);
-	STAssertTrue([string cw_isNotNil], nil);
-}
-
-@end
+SpecEnd
