eaae1431641e7b1f5d0fc8a7dcbf817e14f3fed1
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-25 09:16:47 -0600

    unit tests for NSNumber Additions

---
+++ Source/NSNumberAdditionsTests.m
@@ -0,0 +1,30 @@
+//
+//  NSNumberAdditionsTests.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 2/24/13.
+//
+//
+
+#import "NSNumberAdditionsTests.h"
+#import "NSNumberAdditions.h"
+
+SpecBegin(CWNSNumberAdditions)
+
+it(@"should repeatedly execute the block an expected amount of times", ^{
+	__block NSUInteger count = 0;
+	[@8 cw_times:^{
+		++count;
+	}];
+	expect(count == 8).to.beTruthy();
+});
+
+it(@"should not execute at all when 0", ^{
+	__block NSUInteger count = 0;
+	[@0 cw_times:^{
+		++count;
+	}];
+	expect(count == 0).to.beTruthy();
+});
+
+SpecEnd
