fabde234dc2a2c9aaea429b7c165b55d7ea65b3d
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-26 09:32:03 -0600

    update block parameters

--- Source/NSNumberAdditionsTests.m
+++ Source/NSNumberAdditionsTests.m
@@ -13,7 +13,7 @@ SpecBegin(CWNSNumberAdditions)
 
 it(@"should repeatedly execute the block an expected amount of times", ^{
 	__block NSUInteger count = 0;
-	[@8 cw_times:^{
+	[@8 cw_times:^(NSInteger index, BOOL *stop) {
 		++count;
 	}];
 	expect(count == 8).to.beTruthy();
@@ -21,7 +21,7 @@ it(@"should repeatedly execute the block an expected amount of times", ^{
 
 it(@"should not execute at all when 0", ^{
 	__block NSUInteger count = 0;
-	[@0 cw_times:^{
+	[@0 cw_times:^(NSInteger index, BOOL *stop) {
 		++count;
 	}];
 	expect(count == 0).to.beTruthy();
