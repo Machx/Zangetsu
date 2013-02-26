fb89f32bd4f6616b424549044e79e47f88351e33
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-26 09:32:35 -0600

    unit test block stop pointer

--- Source/NSNumberAdditionsTests.m
+++ Source/NSNumberAdditionsTests.m
@@ -27,4 +27,13 @@ it(@"should not execute at all when 0", ^{
 	expect(count == 0).to.beTruthy();
 });
 
+it(@"should stop when the stop pointer is set to YES", ^{
+	__block NSUInteger count = 0;
+	[@30 cw_times:^(NSInteger index, BOOL *stop) {
+		++count;
+		if (count == 5) *stop = YES;
+	}];
+	expect(count == 5).to.beTruthy();
+});
+
 SpecEnd
