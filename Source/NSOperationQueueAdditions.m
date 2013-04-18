977a2283a6a1a87ea5fdd9e6999974e735d0211d
Colin Wheeler <cocoasamurai@gmail.com>
2013-04-18 09:20:45 -0500

    weak/strong ref

--- Source/NSOperationQueueAdditions.m
+++ Source/NSOperationQueueAdditions.m
@@ -7,15 +7,19 @@
 //
 
 #import "NSOperationQueueAdditions.h"
+#import <EXTScope.h>
 
 @implementation NSOperationQueue (NSOperationQueueAdditions)
 
 -(void)cw_addOperationAfterDelay:(double)seconds
                        withBlock:(dispatch_block_t)block {
+	
     NSParameterAssert(block);
+	@weakify(self);
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW,(seconds * NSEC_PER_SEC));
 	dispatch_queue_t queue = dispatch_queue_create("com.Zangetsu.NSOperationQueueAdditions", 0);
     dispatch_after(popTime, queue, ^(void){
+		@strongify(self);
         [self addOperationWithBlock:block];
     });
 	dispatch_release(queue);
