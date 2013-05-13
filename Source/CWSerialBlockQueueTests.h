89fde796e2b3c887b5b7bac63807ba2d165e64b0
Colin Wheeler <cocoasamurai@gmail.com>
2013-05-13 09:01:09 -0500

    remove CWSerialBlockQueue tests

--- Source/CWSerialBlockQueueTests.h
+++
@@ -1,9 +0,0 @@
-//
-//  CWLightBlockQueueTests.h
-//  Zangetsu
-//
-//  Created by Colin Wheeler on 2/24/12.
-//  Copyright (c) 2012. All rights reserved.
-//
-
-#import <SenTestingKit/SenTestingKit.h>
