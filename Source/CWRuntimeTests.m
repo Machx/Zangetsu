6787afb7f54dd761f7b8312c2146b82e5db8ce1b
Colin Wheeler <cocoasamurai@gmail.com>
2013-05-24 10:39:37 -0500

    unit test swizzle class methods

--- Source/CWRuntimeTests.m
+++ Source/CWRuntimeTests.m
@@ -11,9 +11,14 @@
 static const NSInteger code1 = 100;
 static const NSInteger code2 = 200;
 
+static const NSInteger classErrorCode1 = 300;
+static const NSInteger classErrorCode2 = 400;
+
 @interface CWTestFoo : NSObject
 -(void)setError1:(NSError **)error;
 -(void)setError2:(NSError **)error;
++(void)setClassError1:(NSError **)error;
++(void)setClassError2:(NSError **)error;
 @end
 
 @implementation CWTestFoo
@@ -26,6 +31,17 @@ static const NSInteger code2 = 200;
 -(void)setError2:(NSError **)error {
 	if (error) {
 		*error = [NSError errorWithDomain:@"domain" code:code2 userInfo:nil];
+	}
+}
+
++(void)setClassError1:(NSError **)error {
+	if (error) {
+		*error = [NSError errorWithDomain:@"domain" code:classErrorCode1 userInfo:nil];
+	}
+}
++(void)setClassError2:(NSError **)error {
+	if (error) {
+		*error = [NSError errorWithDomain:@"domain" code:classErrorCode2 userInfo:nil];
 	}
 }
 
@@ -59,6 +75,28 @@ it(@"should swizzle instance methods", ^{
 	[foo setError2:&error4];
 	expect(error4.domain).to.equal(@"domain");
 	expect(error4.code == code1).to.beTruthy();
+});
+
+it(@"should swizzle class methods", ^{
+	NSError *error1;
+	[CWTestFoo setClassError1:&error1];
+	expect(error1.code == classErrorCode1).to.beTruthy();
+	
+	NSError *error2;
+	[CWTestFoo setClassError2:&error2];
+	expect(error2.code == classErrorCode2).to.beTruthy();
+	
+	NSError *swizzleError;
+	CWSwizzleClassMethods([CWTestFoo class], @selector(setClassError1:), @selector(setClassError2:), &swizzleError);
+	if(swizzleError != nil) NSLog(@"Swizzle Error: %@",swizzleError);
+	
+	NSError *error3;
+	[CWTestFoo setClassError1:&error3];
+	expect(error3.code == classErrorCode2).to.beTruthy();
+	
+	NSError *error4;
+	[CWTestFoo setClassError2:&error4];
+	expect(error4.code == classErrorCode1).to.beTruthy();
 });
 
 SpecEnd
