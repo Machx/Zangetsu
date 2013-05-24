960d92abe1b13c0eea5d70f38a19bfeaa33c97b6
Colin Wheeler <cocoasamurai@gmail.com>
2013-05-24 10:25:56 -0500

    Add CWRuntimeTests for swizzling methods

---
+++ Source/CWRuntimeTests.h
@@ -0,0 +1,13 @@
+//
+//  CWRuntimeTests.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 5/24/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWRuntimeTests : SenTestCase
+
+@end
---
+++ Source/CWRuntimeTests.m
@@ -0,0 +1,13 @@
+//
+//  CWRuntimeTests.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 5/24/13.
+//
+//
+
+#import "CWRuntimeTests.h"
+
+@implementation CWRuntimeTests
+
+@end
