cb1b8cd9aadd7f9c06fd7a65cd4f8fbe6c9bcb20
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-13 09:08:31 -0500

    add map

--- Source/NSArray+Transform.h
+++ Source/NSArray+Transform.h
@@ -1,13 +1,53 @@
-//
+/*
 //  NSArray+Transform.h
 //  Zangetsu
 //
 //  Created by Colin Wheeler on 8/13/13.
 //
 //
+ 
+ Copyright (c) 2013, Colin Wheeler
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <Foundation/Foundation.h>
 
-@interface NSArray (Transform)
+@interface NSArray (Zangetsu_Transform)
+
+/**
+ Map objects in one array to another array
+ 
+ cw_mapArray basically maps an array by enumerating over the array to be mapped
+ and executes the block while passing in the object to map. You simply need to
+ either
+ (1) return the object to be mapped in the new array or
+ (2) return nil if you don't want to map the passed in object
+ 
+ @param block a block in which you return an object to be mapped to a new array
+ or nil to not map it
+ @return a new mapped array
+ */
+-(NSArray *)cw_mapArray:(id (^)(id object))block;
 
 @end
