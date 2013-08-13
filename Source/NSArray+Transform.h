197c03cddb2c5fb6ab741d064ec670aa4b40ce1f
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-13 09:08:13 -0500

    NSArray+Transform

---
+++ Source/NSArray+Transform.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Transform.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/13/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Transform)
+
+@end
---
+++ Source/NSArray+Transform.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+Transform.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/13/13.
+//
+//
+
+#import "NSArray+Transform.h"
+
+@implementation NSArray (Transform)
+
+@end
