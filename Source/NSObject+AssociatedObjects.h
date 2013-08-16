b606180d990512bfff95b3ad2ffc3a32cd76c89e
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-16 09:12:16 -0500

    add associated objects category files

---
+++ Source/NSObject+AssociatedObjects.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+AssociatedObjects.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/16/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (AssociatedObjects)
+
+@end
---
+++ Source/NSObject+AssociatedObjects.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+AssociatedObjects.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/16/13.
+//
+//
+
+#import "NSObject+AssociatedObjects.h"
+
+@implementation NSObject (AssociatedObjects)
+
+@end
