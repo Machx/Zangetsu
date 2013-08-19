97f638630c3be6cbe6c27f4199f74b32f6c06e57
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-19 09:02:58 -0500

    add +transform files

---
+++ Source/NSDictionary+Transform.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Transform.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/19/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Transform)
+
+@end
---
+++ Source/NSDictionary+Transform.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Transform.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/19/13.
+//
+//
+
+#import "NSDictionary+Transform.h"
+
+@implementation NSDictionary (Transform)
+
+@end
