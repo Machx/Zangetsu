8ecaae22652c54aac86635c85195fcb870055d2b
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-20 10:15:48 -0500

    remove from the old

--- Source/NSObjectAdditions.h
+++ Source/NSObjectAdditions.h
@@ -35,57 +35,6 @@
 
 @interface   NSObject (CWNSObjectAdditions)
 
-//Block Timing Methods -
-
-/**
- Executes the passed in block after a specified time delay
- 
- @param delay time to delay executing block after
- @param block to be executed after the time delay
- */
--(void)cw_performAfterDelay:(NSTimeInterval)delay
-				  withBlock:(dispatch_block_t)block;
-
-//Queueing  Methods -
-
-/**
- Performs selector with obj as an argument on queue using NSInvocationOperation
- 
- This method creates a NSInvocationOperationw with itself as the target and
- using the select/obj arguments on a specified NSOperationQueue.
- 
- @param selector the selector to be performed
- @param obj an optional object arguemnt for selector
- @param queue the NSOperationQueue to invoke a NSInvocationOperation on
- */
--(void)cw_performSelector:(SEL)selector
-			   withObject:(id)obj
-				  onQueue:(NSOperationQueue *)queue;
-
-/**
- This method calls dispatch_async() using queue and then invokes selector
- 
- If any of the arguments are nil/NULL then the method will throw an assertion.
- 
- @param selector the selector to be performed on queue. Cannot be NULL.
- @param obj an optional object argument. Cannot be nil.
- @param queue the queue to be used for the dispatch_async() call.Cannot be NULL.
- */
--(void)cw_performSelector:(SEL)selector
-			   withObject:(id)obj
-			   onGCDQueue:(dispatch_queue_t)queue;
-
-/**
- Calls -performSelector but directs clang to ignore a potential leak
- 
- This tells clang to ignore a potential leak when calling -performSelector
- just for the duration of this one call.
- 
- @param selector the selector to be called
- @return id the object returned from NSObject -performSelector: method
- */
--(id)cw_ARCPerformSelector:(SEL)selector;
-
 /**
  Returns YES on any non nil object, nil objects will return NO
  
--- Source/NSObjectAdditions.m
+++ Source/NSObjectAdditions.m
@@ -34,63 +34,7 @@
 #import "NSObjectAdditions.h"
 #import <objc/runtime.h>
 
-@interface NSObject(CWPrivateNSObjectAdditions)
--(void)_cw_blockInvokeCallBack;
-@end
-
-
 @implementation NSObject (CWNSObjectAdditions)
-
-#pragma mark - Exerimental Perform with Block Methods
-
--(void)cw_performAfterDelay:(NSTimeInterval)delay
-				  withBlock:(dispatch_block_t)block {
-	[block performSelector:@selector(_cw_blockInvokeCallBack) 
-				withObject:nil 
-				afterDelay:delay];
-}
-
-/**
- Private - Internal Implementation Method
- */
--(void)_cw_blockInvokeCallBack {
-	dispatch_block_t block = (id)self;
-	block();
-}
-
-#pragma mark - Perform Selector Methods
-
--(void)cw_performSelector:(SEL)selector 
-			   withObject:(id)obj 
-				  onQueue:(NSOperationQueue *)queue {
-	CWAssert(queue != nil);
-	NSInvocationOperation *op = [[NSInvocationOperation alloc] initWithTarget:self
-																	 selector:selector
-																	   object:obj];
-	[queue addOperation:op];
-}
-
--(void)cw_performSelector:(SEL)selector 
-			   withObject:(id)obj 
-			   onGCDQueue:(dispatch_queue_t)queue {
-	CWAssert(queue != NULL);
-	CWAssert(obj != nil);
-	CWAssert(selector != NULL);
-	dispatch_async(queue, ^{
-		[self performSelector:selector 
-				   withObject:obj 
-				   afterDelay:0];
-	});
-}
-
--(id)cw_ARCPerformSelector:(SEL)selector {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-	return [self performSelector:selector];
-#pragma clang diagnostic pop
-}
-
-#pragma mark - Misc
 
 -(BOOL)cw_isNotNil {
 	return YES;
