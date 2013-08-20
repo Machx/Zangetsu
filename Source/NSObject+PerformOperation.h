b583a987c68a8680fc5c4e39934df26fcac79d1f
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-20 10:14:54 -0500

    add NSObject performoperation

---
+++ Source/NSObject+PerformOperation.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+PerformOperation.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/20/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (PerformOperation)
+
+@end
---
+++ Source/NSObject+PerformOperation.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+PerformOperation.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/20/13.
+//
+//
+
+#import "NSObject+PerformOperation.h"
+
+@implementation NSObject (PerformOperation)
+
+@end
