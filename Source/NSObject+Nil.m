a83435417f01b3c61d2fb674660930bc07ed93ec
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-23 09:33:28 -0500

    NSObjectAdditions => NSObject+Nil

--- Source/NSObject+Nil.m
+++ Source/NSObject+Nil.m
@@ -31,7 +31,7 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "NSObjectAdditions.h"
+#import "NSObject+Nil.h"
 #import <objc/runtime.h>
 
 @implementation NSObject (CWNSObjectAdditions)
