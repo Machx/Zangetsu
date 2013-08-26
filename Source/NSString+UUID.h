98e079c358a10a047ad17fe44ec24b5d1a271829
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-26 11:43:41 -0500

    new UUID category on NSString

---
+++ Source/NSString+UUID.h
@@ -0,0 +1,57 @@
+/*
+//  NSString+UUID.h
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 8/26/13.
+//
+//
+ 
+ Copyright (c) 2013, Colin Wheeler
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ Returns a unique NSString prepended by the passed in string
+ 
+ @param preString the string to be prepended on the unique string
+ @return NSString with a UUID String prepended with the prestring argument
+ */
+NSString *CWUUIDStringPrependedWithString(NSString *preString);
+
+@interface NSString (Zangetsu_NSString_UUID)
+
+/**
+ Returns a UUID String
+ 
+ On OS X 10.8 and later this method calls the new [[NSUUID UUID] UUIDString]
+ function. For 10.7 compatibility this method calls CFUUIDCreateString() and
+ bridges that to a NSString.
+ 
+ @return a new UUID NSString instance
+ */
++ (NSString *)cw_uuidString;
+
+@end
