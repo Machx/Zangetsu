e410f155e83d11fc74cad8654ea87829b180e0c2
Colin Wheeler <cocoasamurai@gmail.com>
2013-09-01 10:51:51 -0500

    NSMutableURLRequestAdditions => NSMutableURLRequest+Authorization

--- Source/NSMutableURLRequest+Authorization.m
+++ Source/NSMutableURLRequest+Authorization.m
@@ -31,7 +31,7 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#import "NSMutableURLRequestAdditions.h"
+#import "NSMutableURLRequest+Authorization.h"
 
 static NSString *const kCWHttpAuthorizationHeaderField = @"Authorization";
 
