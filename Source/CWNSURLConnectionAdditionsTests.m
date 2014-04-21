4600e1ed52281a14f4d7cefc1e9c58df44be3f7e
Colin Wheeler <cocoasamurai@gmail.com>
2014-04-20 17:03:07 -0700

    Unit tests for NSURLConnection Additions

---
+++ Source/CWNSURLConnectionAdditionsTests.m
@@ -0,0 +1,55 @@
+//
+//  CWNSURLConnectionAdditionsTests.m
+//  Zangetsu
+//
+//  Created by Colin Wheeler on 4/20/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "NSURLConnection+Asynchronous.h"
+
+static NSString * const kAppleResponse = @"All Glory to the Hypnotoad";
+
+SpecBegin(NSURLConnectionAdditions)
+
+beforeAll(^{
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"www.apple.com"];
+    } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        NSData *data = [kAppleResponse dataUsingEncoding:NSUTF8StringEncoding];
+        return [OHHTTPStubsResponse responseWithData:data statusCode:200 headers:nil];
+    }];
+});
+
+afterAll(^{
+    [OHHTTPStubs removeAllStubs];
+});
+
+it(@"should return the expected response from a synchronous request", ^{
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]];
+    NSURLResponse *response;
+    NSError *error;
+    NSData *data = [NSURLConnection cw_performGCDSynchronousRequest:request
+                                                           response:&response
+                                                              error:&error];
+    
+    NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    expect(responseString).to.equal(kAppleResponse);
+});
+
+it(@"should return the expected response from an ansyncrhonous request", ^{
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]];
+    
+    __block NSString *responseString;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    [NSURLConnection cw_performAsynchronousRequest:request onGCDQueue:queue completionBlock:^(NSData *data, NSURLResponse *response, NSError *error) {
+        responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }];
+    
+    expect(responseString).will.equal(kAppleResponse);
+});
+
+SpecEnd
